6c9163cade3bb79c48d8d9c94734b4d576deed56
bananasyndro <bananasyndro@naver.com>
2024-02-14 01:17:26 +0900

    2024-02-14

--- src/main/java/com/controller/jin/FrontController.java
+++ src/main/java/com/controller/jin/FrontController.java
@@ -32,20 +32,16 @@ import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.FileCopyUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+
 import org.springframework.web.multipart.MultipartFile;
 
-import com.dao.jin.LocationDao;
 import com.dto.jin.UserDto;
-import com.dto.jin.User_fileDto;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
